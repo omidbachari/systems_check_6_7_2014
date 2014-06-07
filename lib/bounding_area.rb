8d266046d5886460fce616106b10bb6c0ec88fb9
Omid Omid <omidbachari@gmail.com>
2014-06-07 10:57:24 -0400

    Figured out proper method to check for empty array of boxes

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,21 +1,27 @@
 require_relative "bounding_box"
+require "pry"
 
 class BoundingArea
-  def initialize(box_1 = 0, box_2 = 0, box_3 = 0, box_4 = 0, box_5 = 0)
-    @box_1 = box_1
-    @box_2 = box_2
-    @box_3 = box_3
-    @box_4 = box_4
-    @box_5 = box_5
+  def initialize(array_of_boxes = nil)
+    @array_of_boxes = array_of_boxes
   end
 
-  def contains_point?(x, y)
-
+  def empty?
+    if @array_of_boxes.count == 0
+      @rez = true
+    end
+    @rez
   end
+
+  # def contains_point?(x, y)
+  #   if empty?
+  #     @res = false
+  #   else
+  #     @res = nil
+  #     @array_of_boxes.each do |box|
+  #       if box.contains_point?(x,y)
+
+  # end
 
 end
 
-
-# Create a BoundingArea class that represents a collection of individual bounding boxes. It should implement the following method:
-
-# contains_point?(x, y) returns true if the given (x, y) coordinate is contained within any of the bounding boxes for this area.
